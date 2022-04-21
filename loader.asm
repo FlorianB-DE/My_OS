569e3904edf65a4c8e36d23b3193cbe0226daeae
Florian <florian.becker-f86@ruhr-uni-bochum.de>
2022-04-21 11:25:07 +0200

    Initial commit

---
+++ loader.asm
@@ -0,0 +1,59 @@
+org 0x7C00
+bits 16
+
+jmp main
+
+;
+; prints a string to the screen
+; params:
+;   - ds:si points to string
+;
+
+puts:
+    ; save registers we will modify
+    push si
+    push ax
+
+.loop:
+    lodsb                   ; loads next character in al
+    or al, al               ; verify if next character is null
+    jz .done
+
+    mov ah, 0x0e            ; call bios interrupt
+
+    mov bh, 0
+    int 0x10
+
+
+    jmp .loop
+
+.done:
+    pop ax
+    pop si
+    ret
+
+
+main:
+
+    ; setup data segments
+    mov ax, 0
+    mov ds, ax
+    mov es, ax
+
+    ; setup stack
+    mov ss, ax
+    mov sp, 0x700 ; stack grows downwards from where we are loaded in memory
+
+    ; print message
+    mov si, msg_hello
+    call puts
+
+    hlt
+
+.halt:
+    jmp .halt
+
+msg_hello: db 'Hello World!', 0x0D, 0x0A, 0
+
+times 510-($-$$) db 0
+dw 0AA55h
